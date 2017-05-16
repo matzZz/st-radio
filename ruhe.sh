a38fab369f6a566e54cfcecee769a31a12360da7
matzZz <matzZz@users.noreply.github.com>
2017-05-16 13:21:43 +0200

    Update ruhe.sh

--- ruhe.sh
+++ ruhe.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo killall mplayer < /dev/null
 echo "MPlayer-Prozesse beendet."
-sudo rm -r ./status/*.txt
-echo "Status geloescht."
+#sudo rm -r ./status/*.txt
+echo "0" | sudo tee /var/www/html/status/*.txt
+echo "Status 0."
